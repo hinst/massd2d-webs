d714a1d7eb235b47c981b7d2259d66b13b84f3f1
hinst <hinstance@yandex.ru>
2017-09-09 18:21:34 +0300

    set 0 time zone at startup

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,13 @@
 package hinst.massd2d.webs
 
 import com.mashape.unirest.http.Unirest
+import java.util.*
 
 class Main
 
 fun main(args: Array<String>) {
     println("STARTING... appMainPath=" + appMainPath)
+    TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
     val app = if (args.size > 0) App(configFileName = args[0]) else App()
     app.run()
     Unirest.shutdown()
