f55a65cd20dc0cae36185b4a0b3f78daff676477
hinst <hinstance@yandex.ru>
2017-09-09 13:32:50 +0300

    date

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,10 @@
 package hinst.massd2d.webs
 
 import com.mashape.unirest.http.Unirest
-import java.util.*
 
 class Main
 
 fun main(args: Array<String>) {
-    if (false) {
-        println(dateToStringDirect(Date()))
-        return
-    }
     println("STARTING... appMainPath=" + appMainPath)
     val app = if (args.size > 0) App(configFileName = args[0]) else App()
     app.run()
